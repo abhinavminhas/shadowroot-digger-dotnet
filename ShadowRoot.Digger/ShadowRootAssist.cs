99b51929b495e547fe492bcd11400ded6e6e669e
abhinavminhas <abhinavminhas@users.noreply.github.com>
2022-02-02 17:45:58 +1100

    ShadowRootAssist - Removed unused references

--- ShadowRoot.Digger/ShadowRootAssist.cs
+++ ShadowRoot.Digger/ShadowRootAssist.cs
@@ -1,8 +1,6 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 

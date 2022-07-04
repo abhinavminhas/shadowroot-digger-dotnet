57d90430b186974f8fc98918e261ac192470cbc8
abhinavminhas <abhinavminhas@users.noreply.github.com>
2022-07-04 22:25:49 +1000

    Bump Chromedriver version '102.0.5005.61' -> '103.0.5060.53'

--- ShadowRootDigger.CORE.Tests/TestBase.cs
+++ ShadowRootDigger.CORE.Tests/TestBase.cs
@@ -14,7 +14,7 @@ namespace ShadowRootDigger.CORE.Tests
     public class TestBase
     {
         internal IWebDriver WebDriver;
-        private readonly string _chromeDriverVersion = "102.0.5005.61";
+        private readonly string _chromeDriverVersion = "103.0.5060.53";
         protected const string TESTS_DOTNETCORE = "TESTS-DOTNETCORE";
         protected const string DOTNETCORE_CHROME_SETTINGS = "DOTNETCORE-CHROME-SETTINGS";
         protected const string DOTNETCORE_SHADOW_DOM_HTML = "DOTNETCORE-SHADOW-DOM-HTML";
